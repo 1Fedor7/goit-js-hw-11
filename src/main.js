9b533847dd6a907a9f24cccc8173a1119545ae17
Fedor <fiafedor7@gmail.com>
2024-09-26 18:31:36 +0200

    Update main.js

---
+++ src/main.js
@@ -0,0 +1,48 @@
+import { fetchImages } from './pixabay-api';
+import { renderImages, clearGallery, showNotification } from './render-functions';
+
+const form = document.querySelector('.search-form');
+const input = document.querySelector('input[name="searchQuery"]');
+const loader = document.querySelector('.loader');
+
+let currentPage = 1;
+let currentQuery = '';
+
+function showLoader() {
+  loader.classList.add('visible');
+}
+
+function hideLoader() {
+  loader.classList.remove('visible');
+}
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const query = input.value.trim();
+  if (!query) {
+    showNotification('Please enter a search term.');
+    return;
+  }
+
+  currentQuery = query;
+  currentPage = 1;
+  
+  clearGallery();
+  showLoader();
+
+  try {
+    const images = await fetchImages(currentQuery, currentPage);
+    hideLoader();
+
+    if (images.length === 0) {
+      showNotification('Sorry, there are no images matching your search query. Please try again!');
+      return;
+    }
+
+    renderImages(images);
+  } catch (error) {
+    hideLoader();
+    showNotification('Something went wrong. Please try again later.');
+  }
+});
